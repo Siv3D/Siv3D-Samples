cf936cdb8709dd7ff56a982ea4ee756a105b9642
Ryo Suzuki <reputeless+github@gmail.com>
2022-10-21 22:38:59 +0900

    Update Main.cpp

--- Samples/FocusArrows/Main.cpp
+++ Samples/FocusArrows/Main.cpp
@@ -4,14 +4,18 @@ void Main()
 {
 	Window::Resize(1280, 720);
 
+	// 2D ⇔ 3D の切り替えトランジション
 	Transition transition{ 0.3s, 0.3s };
 
 	while (System::Update())
 	{
+		// マウスを押していると 3D になる
 		transition.update(MouseL.pressed());
 
+		// 3D にする割合
 		const double exp = (1.0 + transition.easeOut(Easing::Quad));
 
+		// 加算ブレンド
 		const ScopedRenderStates2D bled{ BlendState::Additive };
 
 		for (int32 y = 0; y < (720 / 120 + 1); ++y)
@@ -32,6 +36,7 @@ void Main()
 
 				const ColorF color = HSV{ (x * 155 + y * 135) };
 
+				// 矢印を描く
 				Shape2D::Arrow(from, to, 15, Vec2{ 30.0, 50.0 }).draw(ColorF{ color, 0.4 }).drawFrame(3, color);
 			}
 		}
